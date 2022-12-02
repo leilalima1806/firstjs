bf70f2f0817ea9a03856522101f0c65700f21aa2
leilalima1806 <101912424+leilalima1806@users.noreply.github.com>
2022-12-02 12:19:22 -0300

    Delete lista de exercicio directory

--- lista de exercicio/exe2.js
+++
@@ -1,9 +0,0 @@
-function ex2(qual) {
-    var c = parseInt(document.getElementById("c").value)
-    var f = parseInt(document.getElementById("f").value)
-    if (qual == 'c') {
-        document.getElementById("f").value = (c * 1.8) + 32
-    } else {
-        document.getElementById("c").value = (f - 32) / 1.8
-    }
-}
--- lista de exercicio/exe4.js
+++
@@ -1,8 +0,0 @@
-function calcular() {
-
-    var km = parseInt(document.getElementById('km').value);
-    var com = parseFloat(document.getElementById('combustivel').value);
-
-    var dista = (km / 8);
-    document.getElementById('res').innerHTML = (dista * com + " reais");
-}
